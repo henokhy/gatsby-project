d5a31f31db30dd436617cd949fd1d6a330c75c30
SVMLPSB <henokhy@users.noreply.github.com>
2021-05-29 15:01:59 +0700

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,7 +26,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Cara - @lekoarts/gatsby-theme-cara`,
+        name: `Henokhy - @lekoarts/gatsby-theme-cara`,
         short_name: `Henokhy`,
         description: `Playful and Colorful One-Page portfolio featuring Parallax effects and animations`,
         start_url: `/`,
